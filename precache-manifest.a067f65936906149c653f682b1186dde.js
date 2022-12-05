5e49f45b2260e3b7c270e941a9362843fb475f58
Igor <igorjoha@email.ee>
2022-12-05 21:39:13 +0200

    Updates

---
+++ precache-manifest.a067f65936906149c653f682b1186dde.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d119d060d7fef01ffaa574ad04591ad7",
+    "url": "/exam-front/index.html"
+  },
+  {
+    "revision": "388b65c78042a873e163",
+    "url": "/exam-front/static/css/main.bb1cd6a4.chunk.css"
+  },
+  {
+    "revision": "313ccda13af906be5d2b",
+    "url": "/exam-front/static/js/2.493e2401.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/exam-front/static/js/2.493e2401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "388b65c78042a873e163",
+    "url": "/exam-front/static/js/main.d71ab827.chunk.js"
+  },
+  {
+    "revision": "1e71627c7011d478a555",
+    "url": "/exam-front/static/js/runtime-main.8101db39.js"
+  }
+]);
